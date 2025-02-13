b7661ec864e10f9c011ce9cdcde8c0cf112f5191
Mathildezzz <155410896+Mathildezzz@users.noreply.github.com>
2025-02-13 18:17:21 +0800

    增加新客字段；修改判定wmp引流逻辑

--- wos_converted_through_wmp_by_transaction_year.sql
+++ wos_converted_through_wmp_by_transaction_year.sql
@@ -35,7 +35,15 @@ LEFT JOIN (SELECT DISTINCT parent_order_id, order_scenario FROM dm_view.offline_
       and source_channel in ('LCS')
       and date(tr.order_paid_date) < current_date
       and ((tr.source_channel = 'LCS' and sales_type <> 3) or (tr.source_channel in ('TMALL', 'DOUYIN', 'DOUYIN_B2B') and tr.order_type = 'normal')) -- specific filtering for LCS, TM and DY
-    )
+    ),
+    
+    
+new_member AS (
+  SELECT DISTINCT member_detail_id
+     FROM edw.d_member_detail
+     WHERE  1= 1
+      AND extract('year' FROM DATE(join_time)) = extract('year' FROM current_date)
+  )
     
 select  
      extract('year' FROM order_paid_date)                                                                                                                                                                                 AS transaction_year,
@@ -45,16 +53,21 @@ select
      CAST((sum(case when is_member_order IS TRUE AND sales_qty > 0 then order_rrp_amt else 0 end) - sum(case when is_member_order IS TRUE AND sales_qty < 0 then abs(order_rrp_amt) else 0 end)) AS FLOAT)/NULLIF(count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true then trans.parent_order_id else null end),0)                  AS wos_member_atv_ttl,
      CAST((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true then trans.parent_order_id else null end)) AS FLOAT) / NULLIF((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true then trans.member_detail_id else null end)) ,0)                                                                          AS wos_member_frequency_ttl,
      
-     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' then trans.member_detail_id else null end)),0)                                                                                                                AS wos_member_shopper_from_wmp,
-     CAST((sum(case when is_member_order IS TRUE AND sales_qty > 0 AND order_scenario = 'WeCom' then order_rrp_amt else 0 end) - sum(case when is_member_order IS TRUE AND sales_qty < 0 AND order_scenario = 'WeCom' then abs(order_rrp_amt) else 0 end)) AS FLOAT)                AS wos_member_sales_from_wmp,
-     CAST((sum(case when sales_qty > 0 AND order_scenario = 'WeCom' then order_rrp_amt else 0 end) - sum(case when sales_qty < 0 AND order_scenario = 'WeCom' then abs(order_rrp_amt) else 0 end)) AS FLOAT)                                                                        AS wos_ttl_sales_from_wmp,
-     CAST((sum(case when is_member_order IS TRUE AND sales_qty > 0 AND order_scenario = 'WeCom' then order_rrp_amt else 0 end) - sum(case when is_member_order IS TRUE AND sales_qty < 0 AND order_scenario = 'WeCom' then abs(order_rrp_amt) else 0 end)) AS FLOAT)/NULLIF(count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' then trans.parent_order_id else null end),0)   AS wos_member_atv_from_wmp,
-     CAST((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' then trans.parent_order_id else null end)) AS FLOAT) / NULLIF((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' then trans.member_detail_id else null end)) ,0)                                                                     AS wos_member_frequency_from_wmp,
+     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' then trans.member_detail_id else null end)),0)                                                                                                                AS wos_member_shopper_from_wmp,
+     CAST((sum(case when is_member_order IS TRUE AND sales_qty > 0 AND order_scenario = 'Organic' then order_rrp_amt else 0 end) - sum(case when is_member_order IS TRUE AND sales_qty < 0 AND order_scenario = 'Organic' then abs(order_rrp_amt) else 0 end)) AS FLOAT)                AS wos_member_sales_from_wmp,
+     CAST((sum(case when sales_qty > 0 AND order_scenario = 'Organic' then order_rrp_amt else 0 end) - sum(case when sales_qty < 0 AND order_scenario = 'Organic' then abs(order_rrp_amt) else 0 end)) AS FLOAT)                                                                        AS wos_ttl_sales_from_wmp,
+     CAST((sum(case when is_member_order IS TRUE AND sales_qty > 0 AND order_scenario = 'Organic' then order_rrp_amt else 0 end) - sum(case when is_member_order IS TRUE AND sales_qty < 0 AND order_scenario = 'Organic' then abs(order_rrp_amt) else 0 end)) AS FLOAT)/NULLIF(count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' then trans.parent_order_id else null end),0)   AS wos_member_atv_from_wmp,
+     CAST((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' then trans.parent_order_id else null end)) AS FLOAT) / NULLIF((count(distinct case when is_member_order IS TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' then trans.member_detail_id else null end)) ,0)                                                                     AS wos_member_frequency_from_wmp,
      
-     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' AND eff_reg_channel = 'BRANDWMP' then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_wmp_reg,
-     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'WeCom' AND eff_reg_channel LIKE '%LCS%' then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_lcs_reg
+     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' AND eff_reg_channel = 'BRANDWMP' then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_wmp_reg,
+     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' AND eff_reg_channel LIKE '%LCS%' then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_lcs_reg,
+
+     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' AND eff_reg_channel = 'BRANDWMP' AND new_member.member_detail_id IS NOT NULL then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_wmp_reg_new,
+     NULLIF((count(distinct case when is_member_order = TRUE AND if_eff_order_tag = true AND order_scenario = 'Organic' AND eff_reg_channel LIKE '%LCS%' AND new_member.member_detail_id IS NOT NULL then trans.member_detail_id else null end)),0)                           AS wos_member_shopper_lcs_reg_new
 
 from omni_trans_fact trans
+LEFT JOIN new_member
+      ON trans.member_detail_id::integer = new_member.member_detail_id::integer
 where 1 = 1
 and payment_type = 'WOS企微小程序-新场景'
 group by 1;
